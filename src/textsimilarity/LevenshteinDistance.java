6c7d45d720425d3b236c1fad87073e8c405c1911
om ji <omji.mcs.du.2014@gmail.com>
2017-02-08 11:50:41 +0530

    Lavenshtein Distance for TextSimilarity

--- src/textsimilarity/LevenshteinDistance.java
+++ src/textsimilarity/LevenshteinDistance.java
@@ -1,12 +1,13 @@
 package textsimilarity;
+
 public class LevenshteinDistance {
 
 	public static void main(String[] args) {
-		String[] data = { " ", " ", "Vladimir_Levenshtein[1965]", "Vladimir_Levenshtein[1965]",
+		String[] inputs = { " ", " ", "P3_Solutions[2016]", "P3_SOLUTIONS[2016]",
 				"The Examples String", "The Example String", "kitten", "sitting", "saturday", "sunday"};
-		for (int i = 0; i < data.length; i += 2)
+		for (int i = 0; i < inputs.length; i += 2)
 			System.out.println(
-					"distance(" + data[i] + ", " + data[i + 1] + ") = " + Levenshteindistance(data[i], data[i + 1]));
+					"distance(" + inputs[i] + ", " + inputs[i + 1] + ") = " + Levenshteindistance(inputs[i], inputs[i + 1]));
 	}
 
 	public static int Levenshteindistance(String S1, String S2) {
@@ -29,46 +30,6 @@ public class LevenshteinDistance {
 		return costs[S2.length()];
 	}
 
-	public int valuePhrase(String s1, String s2) {
-		int valuePhrase = Levenshteindistance(s1, s2);
-		return valuePhrase;
 
-	}
-
-	public int valueWords(String s1, String s2) {
-		String[] wordsS1 = new String[5];
-		String[] wordsS2 = new String[5];
-		wordsS1 = SplitMultiDelims(s1, " _ -");
-		wordsS2 = SplitMultiDelims(s2, " _ -");
-		int valueWords = 0;
-		int thisD = 0;
-		int wordBest = 0;
-		int wordsTotal = 0;
-		for (int word1 = 0; word1 < wordsS1.length; word1++) {
-
-			wordBest = s2.length();
-			for (int word2 = 0; word2 < wordsS2.length; word2++) {
-				thisD = Levenshteindistance(wordsS1[word1], wordsS2[word2]);
-				if (thisD < wordBest)
-					wordBest = thisD;
-				if (thisD == 0)
-					break;
-			}
-			if (thisD == 0)
-				wordsTotal = foundbest(wordsTotal, wordBest);
-		}
-		valueWords = wordsTotal;
-		return valueWords;
-
-	}
-
-	private int foundbest(int wordsTotal, int wordBest) {
-		return wordsTotal + wordBest;
-	}
-
-	private String[] SplitMultiDelims(String s1, String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
